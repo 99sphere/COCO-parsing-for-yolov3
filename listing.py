79cec48fe6deea57ef2dcbcfeff77ecbade65829
99sphere <59161083+99sphere@users.noreply.github.com>
2020-09-05 03:36:02 +0900

    Add files via upload

--- listing.py
+++ listing.py
@@ -1,14 +1,14 @@
 from os import listdir
 from os.path import isfile, join
 import os
-path = os.getcwd()
+path = os.getcwd() + "/03_eunbin"
 files = [f for f in listdir(path) if isfile(join(path, f))]
-#files = [x for x in files if x.find("t") != -1]
+files = [x for x in files if x.find(".jpg") != -1]
 #print(files)
 files = sorted(files)
-f = open(path + "label_list.txt",'a')
-for i in range(len(files)):
-    data = path + "/"+files[i]
+f = open(path + ".txt",'w')
+for i in files:
+    data = path +"/"+ i
     #print(data)
     f.write(data)
     f.write("\n")
--- merge.py
+++ merge.py
@@ -1,36 +1,67 @@
 from os import listdir
 from os.path import isfile, join
 import os
-import cv2
 
 path_0 = os.getcwd() + "/"
-path_label = path_0 + "person_labels"
-path_train = path_0 + "train2017"
-files_label = [f[:-4] for f in listdir(path_label) if isfile(join(path_label, f))]
-files_train = [f[:-4] for f in listdir(path_train) if isfile(join(path_train, f))]
+result_directory = "merge_person+car"
+destination = path_0 + result_directory
+path_person = path_0 + "person_labels"
+path_car = path_0 + "car_labels"
+
+files_person = [f for f in listdir(path_person) if isfile(join(path_person, f))]
+files_car = [f for f in listdir(path_car) if isfile(join(path_car, f))]
 #files = [x for x in files if x.find("t") != -1]
 
-files_label = sorted(files_label)
-files_train = sorted(files_train)
+files_person = sorted(files_person)
+files_car = sorted(files_car)
 
 try:
-	if not os.path.exists(path_0 + "compare_result"):
-	       os.makedirs(path_0 + "compare_result")
+	if not os.path.exists(destination):
+	       os.makedirs(destination)
 except OSError:
-    print('Error: Creating directory of data')
+    print('Error: Creating %s directory' % result_directory)
 
-for i in files_label:
-    for j in files_train:
-        if i == j:
-            print("i: ", i)
-            print("j: ", type(j))
-            img = cv2.imread(path_train  + "/" + j + ".jpg")
-            cv2.imwrite("./compare_result/" + j + ".jpg", img)
-            print("now, saving" + i + ".jpg")
+for i in files_person:
+	if i in files_car:
+		print(i)
+		f_person = open(path_person + "/" + i,'r')
+		f_car = open(path_car + "/" + i,'r')
+		f_new = open(destination + "/" + i,'w')
+		line_p = f_person.readline()
+		while line_p:
+			f_new.write(line_p)
+			print(line_p)
+			line_p = f_person.readline()
+		line_c = f_car.readline()
+		while line_c:
+			f_new.write(line_c)
+			print(line_c)
+			line_c = f_car.readline()
+		f_person.close()
+		f_car.close()
+		f_new.close()
+	else:
+		print(i)
+		f_person = open(path_person + "/" + i,'r')
+		f_new = open(destination + "/" + i,'w')
+		line_p = f_person.readline()
+		while line_p:
+			f_new.write(line_p)
+			print(line_p)
+			line_p = f_person.readline()
+		f_person.close()
+		f_new.close()
 
-        else:
-            pass
-            #print(i + ".jpg" + " is not exist.")
-
-
-print("Finish!")
+for i in files_car:
+	if i in files_person:
+		pass
+	else:
+		f_car = open(path_car + "/" + i,'r')
+		f_new = open(destination + "/" + i,'w')
+		line_c = f_car.readline()
+		while line_c:
+			f_new.write(line_c)
+			print(line_c)
+			line_c = f_car.readline()
+		f_car.close()
+		f_new.close()
